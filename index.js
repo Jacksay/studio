35faa69771233684b38df3a4e844e7f5555c9ac4
jacksay <jacksay@jacksay.com>
2017-10-19 17:40:45 +0200

    Suppression de Bower

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.get('*', (req, res, next) => {
+   console.log(req.url);
+   next();
+})
 
 app.get('/', (req, res) => {
   res.send('TEST')
